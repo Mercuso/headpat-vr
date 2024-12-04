324b87f1b630bd629cf426a4c8a7fde3c523a64b
Alexander <mercusar@gmail.com>
2024-12-05 01:22:42 +0200

    add http server for serving html page with UI

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import sys
 import websockets
+from aiohttp import web
 from pythonosc.osc_server import AsyncIOOSCUDPServer
 import config
 from dispatcher import dispatcher
@@ -19,6 +20,16 @@ async def ws_main():
     async with websockets.serve(register, "localhost", 8765) as server:
         await server.serve_forever()
 
+async def handler(request):
+    html = open('index.html', "r")
+    return web.Response(text=html.read(), content_type='text/html')
+async def http_main():
+    server = web.Server(handler)
+    runner = web.ServerRunner(server)
+    await runner.setup()
+    site = web.TCPSite(runner, 'localhost', 9000)
+    await site.start()
+
 async def heartbeat_main():
     await ping_device()
 
@@ -26,6 +37,7 @@ async def main():
     task1 = asyncio.create_task(osc_main())
     task2 = asyncio.create_task(ws_main())
     task3 = asyncio.create_task(heartbeat_main())
-    await asyncio.gather(task1, task2)
+    task4 = asyncio.create_task(http_main())
+    await asyncio.gather(task1, task2, task3, task4)
 
 asyncio.run(main())
