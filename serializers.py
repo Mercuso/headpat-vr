7280855157e22970ed5b51eb322f1c82c26b4b87
Alexander <mercusar@gmail.com>
2024-12-13 22:09:55 +0200

    send signals for both sides with independent delay measurement

--- serializers.py
+++ serializers.py
@@ -1,6 +1,5 @@
 import struct
-from storage import Storage
 
-def  serialize_collision_data(storage: Storage) -> bytes:
-    data = (round(storage.lvalue*10) << 4) | round(storage.rvalue*10)
+def  serialize_collision_data(lvalue: float, rvalue: float) -> bytes:
+    data = (round(lvalue*10) << 4) | round(rvalue*10)
     return struct.pack('B', data)
--- storage.py
+++ storage.py
@@ -1,7 +1,20 @@
+import time
+
+class Signal():
+    def __init__(self, value: float):
+        self.received_at_ts = time.time()
+        self.sent_at_ts = 0.0
+        self.value = value
+    received_at_ts: float = 0.0
+    sent_at_ts: float = 0.0
+    value: float = 0.0
+
 class Storage():
     # can take values from 0.0 to 1.00
     lvalue: float = 0.0
     rvalue: float = 0.0
+    lsignal: Signal = None
+    rsignal: Signal = None
     last_sent_at_ts: float = 0
     allow_signal_sending: bool = True
     max_intensity: float = 1.0
